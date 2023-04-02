7b0e842d69376f60d3b7c87152dfa95522acd66c
WLWolf5 <43511929+WLWolf5@users.noreply.github.com>
2023-04-03 06:37:40 +0800

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+TEXT_SUC="LEDE编译成功"
+TEXT_FAIL="LEDE编译成功"
+make download -j32
+make -j2 && curl --data chat_id="5433096526" --data "text=$TEXT_SUC" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage" || make -j$(nproc) V=s || curl --data chat_id="5433096526" --data "text=$TEXT_FAIL" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage"
