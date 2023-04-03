fbae23ded29f6f5cfbc073bab935bc24e11ff640
WLWolf5 <43511929+WLWolf5@users.noreply.github.com>
2023-04-03 14:29:21 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,5 +2,4 @@
 TEXT_SUC="LEDE编译成功"
 TEXT_FAIL="LEDE编译失败"
 make download -j32
-make package/mbedtls/compile -j2
-make -j2 && curl --data chat_id="5433096526" --data "text=$TEXT_SUC" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage" || make -j$(nproc) V=s || curl --data chat_id="5433096526" --data "text=$TEXT_FAIL" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage"
+make -j2 || make package/mbedtls/compile -j2 && make -j2 && curl --data chat_id="5433096526" --data "text=$TEXT_SUC" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage" || make -j$(nproc) V=s || curl --data chat_id="5433096526" --data "text=$TEXT_FAIL" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage"
