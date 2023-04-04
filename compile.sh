a5eec637e712e42e7a6bccd555634291cb56da8b
WLWolf5 <43511929+WLWolf5@users.noreply.github.com>
2023-04-04 13:47:21 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-TEXT_SUC="LEDE编译成功"
-TEXT_FAIL="LEDE编译失败"
+WRT_VER="lede"
+TEXT_SUC_LE="LEDE编译成功"
+TEXT_FAIL_LE="LEDE编译失败"
+TEXT_SUC_RB="Robimarko编译成功"
+TEXT_FAIL_RB="Robimarko编译失败"
+
 make download -j32
-make -j2 || make package/mbedtls/compile -j2 && make -j2 && curl --data chat_id="5433096526" --data "text=$TEXT_SUC" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage" || make -j$(nproc) V=s || curl --data chat_id="5433096526" --data "text=$TEXT_FAIL" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage"
+
+if [ "$WRT_VER" == "lede" ]; then
+    make -j2 || make package/mbedtls/compile -j2 && make -j2 && curl --data chat_id="5433096526" --data "text=$TEXT_SUC_LE" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage" || make -j$(nproc) V=s || curl --data chat_id="5433096526" --data "text=$TEXT_FAIL_LE" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage"
+elif [ "$WRT_VER" == "robi" ]; then
+    make -j2 && curl --data chat_id="5433096526" --data "text=$TEXT_SUC_RB" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage" || make -j$(nproc) V=s || curl --data chat_id="5433096526" --data "text=$TEXT_FAIL_RB" "https://api.telegram.org/bot6059390181:AAHCsjxsPfsXAyAvXcRxcOjIVaifDICW5fA/sendMessage"
+fi
